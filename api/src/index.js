e68cd918983b18789f5b0a7b445bc60303387fa2
Vinnys Lins <vinnysfl@hotmail.com>
2019-10-25 19:59:57 -0300

    feat: added dotenv and mongoose

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
