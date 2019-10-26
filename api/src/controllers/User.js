9f97247814a2f05ee9b9174c36b003f7d0d527c3
Vinnys Lins <vinnysfl@hotmail.com>
2019-10-25 23:52:44 -0300

    feat: added bctypt to hash password

--- api/src/controllers/User.js
+++ api/src/controllers/User.js
@@ -1,9 +1,15 @@
+const { hash } = require('bcryptjs');
+
 const User = require('../models/User');
+
+const SALT_ROUNDS = 7;
 
 const createUser = async (req, res) => {
   try {
     const hasEmail = await User.findOne({ email: req.body.email });
     if (hasEmail) return res.status(409).send('E-mail já cadastrado.');
+
+    req.body.password = await hash(req.body.password, SALT_ROUNDS);
 
     const user = await User.create(req.body);
     return res.json(user);
