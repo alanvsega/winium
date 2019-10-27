bb7c943d69e1dd969a3a2d3e7c8ec118531f9d82
Lucas Teixeira <lucas.teixeira@amlabs.com.br>
2019-10-27 21:52:13 -0200

    Added User Actions

---
+++ app/actions/UserActions.js
@@ -0,0 +1,41 @@
+import {
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGOUT_SUCCESS,
+} from '../constants/Actions';
+
+export const request = (payload) => {
+  return {
+    type: USER_REQUEST,
+    payload
+  }
+}
+
+export const receive = (type) => {
+  return {
+    type
+  }
+}
+
+export const automaticLogin = () => {
+  return async (dispatch) => {
+    try {
+    }
+    catch(e) {
+    }
+  }
+}
+
+export const requestLogin = (data) => {
+  return async (dispatch) => {
+    try {
+    }
+    catch(e) {
+    }
+  }
+}
+
+export const logout = () => {
+  return async (dispatch) => {
+  }
+}
