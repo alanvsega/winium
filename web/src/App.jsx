ec38adb0291ee4e6176f43655676e805d02f379c
Marcos Tomaz <marcos.tomaz@siemens.com>
2019-11-03 18:19:10 -0300

    iniciada estrutura do projeto

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import 'style.css';
+
 const App = () => (
-  <div>
-    <h1>winium</h1>
+  <div className="main-container">
+    <nav>
+
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+      
+    </footer>
   </div>
 );
 
