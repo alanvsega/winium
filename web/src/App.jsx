0e288ad970b14389ef057ac0b83c622002327d37
Marcos Vinicius Oliveira Tomaz <marcoshmpm1@gmail.com>
2019-11-03 23:51:05 -0300

    Estrutura inicial do projeto

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './style.css';
 
-import 'style.css';
+import Header from './components/header/index';
 
 const App = () => (
-  <div className="main-container">
-    <nav>
-
-    </nav>
-    <main>
-
-    </main>
-    <footer>
-      
-    </footer>
-  </div>
+  <Router>
+    <div className="main-container">
+      <nav>
+        <Header />
+      </nav>
+      <main>
+        <Route path="/" />
+      </main>
+      <footer />
+    </div>
+  </Router>
 );
 
 export default App;
---
+++ web/src/components/header/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './style.css';
+
+import Logo from '../../assets/WiniumLogoWhite.png';
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <div>
+        <img className="logo-img" src={Logo} alt="It's a match" />
+      </div>
+      <nav className="navigation-menu-container">
+        <a href="./#">
+          Home
+        </a>
+        <a href="./#">
+          Catalog
+        </a>
+        <a href="./#">
+          About
+        </a>
+      </nav>
+      <div className="login-options-container">
+        <h3>Welcome Visitor</h3>
+        <div className="division" />
+        <a href="./#">Login</a>
+        <a href="./#">Sign Up</a>
+      </div>
+    </div>
+  );
+}
