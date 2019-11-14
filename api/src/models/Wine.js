5de3859b1bbc9b70483cf5cca1ee1fa9a378adaf
Vinnys Lins <vinnysfl@hotmail.com>
2019-11-13 21:20:39 -0300

    feat: add images at wine schema

--- api/src/models/Wine.js
+++ api/src/models/Wine.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Wine = mongoose.Schema({
   country: { type: String, required: true },
   designation: { type: String, required: true },
+  images: [String],
   province: { type: String, required: true },
   region_1: String,
   region_2: String,
