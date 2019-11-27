aae7ed24391424a13b61706e5649f6115d062fef
Vinnys Lins <vinnysfl@hotmail.com>
2019-11-26 23:40:54 -0300

    feat: endpoint to get reviews of one month

--- api/src/controllers/Review.js
+++ api/src/controllers/Review.js
@@ -40,7 +40,7 @@ const getReviewsByWine = async (req, res) => {
     const {
       params: { id },
       query: {
-        limit = 15,
+        limit = 12,
         page = 1,
         sort = '-createdAt',
       },
@@ -54,6 +54,49 @@ const getReviewsByWine = async (req, res) => {
       .sort(sort);
 
     const countPromise = Review.countDocuments({ wine: id });
+
+    const [reviews, count] = await Promise.all([reviewsPromise, countPromise]);
+
+    return res.json({
+      pages: Math.ceil(count / limit),
+      reviews,
+      total: count,
+    });
+  } catch (error) {
+    return res.status(500).send('Erro interno no servidor.');
+  }
+};
+
+const getReviewsByMonth = async (req, res) => {
+  try {
+    const {
+      query: {
+        limit = 12,
+        month = new Date().getMonth() + 1,
+        page = 1,
+        sort = '-createdAt',
+        year = new Date().getFullYear(),
+      },
+      userId,
+    } = req;
+
+    const lastDay = new Date(year, month, 0).getDate();
+
+    const query = {
+      user: userId,
+      createdAt: {
+        $gte: new Date(`${year}-${month}-01`),
+        $lte: new Date(`${year}-${month}-${lastDay}T23:59:59.999Z`),
+      },
+    };
+
+    const reviewsPromise = Review.find(query)
+      .limit(Number(limit))
+      .skip(Number(limit * page - limit))
+      .sort(sort)
+      .populate('wine');
+
+    const countPromise = Review.countDocuments(query);
 
     const [reviews, count] = await Promise.all([reviewsPromise, countPromise]);
 
@@ -125,6 +168,7 @@ module.exports = {
   createReview,
   deleteReview,
   getReviews,
+  getReviewsByMonth,
   getReviewsByWine,
   updateReview,
 };
--- api/src/routes/Review.js
+++ api/src/routes/Review.js
@@ -4,11 +4,13 @@ const {
   createReview,
   deleteReview,
   getReviews,
+  getReviewsByMonth,
   getReviewsByWine,
   updateReview,
 } = require('../controllers/Review');
 
 router.get('/reviews', getReviews);
+router.get('/reviews/month', getReviewsByMonth);
 router.get('/reviews/wine/:id', getReviewsByWine);
 router.post('/review', createReview);
 router.patch('/review/:id', updateReview);
